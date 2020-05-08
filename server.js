01fb139b80e54b9261b86afff616782406149b88
Joshua Gibson <flash50k@gmail.com>
2020-05-08 15:44:41 +0100

    added static site

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+var path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,9 +9,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+// Routing
+var dir = __dirname+'/src'; // Path of the index.js but one dir further (public)
+app.use(express.static(dir)); // serve all files in '/var/www/nodeserver/public/'
---
+++ src/assets/js/modal.js
@@ -0,0 +1,49 @@
+			// Get the modal
+            var modal1 = document.getElementById("p1modal");
+            var modal2 = document.getElementById("p2modal");
+            var modal3 = document.getElementById("p3modal");
+			
+			// Get the button that opens the modal
+            var btn1 = document.getElementById("p1js");
+            var btn2 = document.getElementById("p2js");
+            var btn3 = document.getElementById("p3js");
+			
+			// Get the <span> element that closes the modal
+            var span1 = document.getElementsByClassName("close1")[0];
+            var span2 = document.getElementsByClassName("close2")[0];
+            var span3 = document.getElementsByClassName("close3")[0];
+			
+			// When the user clicks the button, open the modal 
+			btn1.onclick = function() {
+                modal1.style.display = "block";
+              }
+            btn2.onclick = function() {
+                modal2.style.display = "block";
+            }
+            btn3.onclick = function() {
+                modal3.style.display = "block";
+            }
+			
+			// When the user clicks on <span> (x), close the modal
+			span1.onclick = function() {
+                modal1.style.display = "none";
+            }
+            span2.onclick = function() {
+                modal2.style.display = "none";
+            }
+            span3.onclick = function() {
+                modal3.style.display = "none";
+			}
+			
+			// When the user clicks anywhere outside of the modal, close it
+			window.onclick = function(event) {
+                if (event.target == modal1) {
+                    modal1.style.display = "none";
+                }
+                else if (event.target == modal2) {
+                    modal2.style.display = "none";
+                }
+                else if (event.target == modal3) {
+                    modal3.style.display = "none";
+                }
+        }
